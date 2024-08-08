fc4182d78dc9d3b925bf297c6d2a1d856b22a12b
jalvarez <Jalvarez@emphasys-software.com>
2024-08-08 00:30:10 -0600

    intallation of required .net packages & creation of needed folders

---
+++ Infraestructure/Data/Sql/DataBaseCreation.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Participant(
+	ParticipantId int PRIMARY KEY,
+	CompanyName varchar(250),
+	Cedula varchar(250),
+	Name varchar(250),
+	Title varchar(250),
+	Email varchar(250),
+	Phone varchar(10)
+);
