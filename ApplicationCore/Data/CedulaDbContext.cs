01a046f4b03bbfadb4cf75ff381d76b93a744e38
jalvarez <Jalvarez@emphasys-software.com>
2024-08-08 00:56:43 -0600

    creation of cedulaDBContext and its entities

---
+++ ApplicationCore/Data/CedulaDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Server;
+
+public partial class CedulaDbContext : DbContext
+{
+    public CedulaDbContext()
+    {
+    }
+
+    public CedulaDbContext(DbContextOptions<CedulaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Participant> Participants { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Data Source=C:\\Users\\diana\\AppData\\Roaming\\DBeaverData\\workspace6\\.metadata\\sample-database-sqlite-1\\CedulaDB.db;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Participant>(entity =>
+        {
+            entity.ToTable("Participant");
+
+            entity.Property(e => e.ParticipantId)
+                .ValueGeneratedNever()
+                .HasColumnType("INT");
+            entity.Property(e => e.Cedula).HasColumnType("varchar(250)");
+            entity.Property(e => e.CompanyName).HasColumnType("varchar(250)");
+            entity.Property(e => e.Email).HasColumnType("varchar(250)");
+            entity.Property(e => e.Name).HasColumnType("varchar(250)");
+            entity.Property(e => e.Phone).HasColumnType("varchar(10)");
+            entity.Property(e => e.Title).HasColumnType("varchar(250)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ApplicationCore/Models/Participant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Server;
+
+public partial class Participant
+{
+    public long ParticipantId { get; set; }
+
+    public string? CompanyName { get; set; }
+
+    public string? Cedula { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+}
